42c055dd0d0a796d0b427013f7fa04fa2dbf6b83
shishovlyosha <32714968+shishovlyosha@users.noreply.github.com>
2017-10-13 12:56:03 +0300

    Update gittest.java

--- src/ua/itea/git/gittest.java
+++ src/ua/itea/git/gittest.java
@@ -2,6 +2,6 @@ package ua.itea.git;
 
 public class gittest {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world like");
     }
 }
